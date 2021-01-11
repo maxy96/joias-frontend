30a0d1b95784c604fca14994f54c2bff6ba6b600
maxy <maxyespindola96@gmail.com>
2021-01-10 22:36:19 -0300

    telefono y mensaje de wpp agregado

--- src/models/persona.tsx
+++ src/models/persona.tsx
@@ -2,15 +2,21 @@ export default class Persona{
     private phone: string;
 
     constructor(private nombre: string, private apellido: string, private email: string){
-        this.phone = '3794040913';
+        this.phone = process.env.REACT_APP_NRO_PHONE || '+5493794040913';
     }
 
     private fullName(): string{
         return this.nombre + ' ' + this.apellido;
     }
 
+    private message(): string{
+        //%0D%0A Salto de linea en wpp
+        return `Hola me llamo ${this.fullName()} y mi correo es ${this.email} 
+        %0D%0A *ESTE MENSAJE SE PUEDE MODIFICAR*`;
+    }
+
     enviarWpp(){
-        window.open(`https://api.whatsapp.com/send?phone=${this.phone}&text=${this.fullName()}`, '_blanck');  
+        window.open(`https://api.whatsapp.com/send?phone=${this.phone}&text=${this.message()}`, '_blanck');  
     }
 
 }
